8735269d933b1c069e8cae3fba014fe0696b0b2f
Dan McCune <megadannn@gmail.com>
2016-11-14 16:04:12 -0500

    fizzed the buzz

--- public/main.js
+++ public/main.js
@@ -1,17 +1,9 @@
-const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
-}
-
-document.addEventListener('DOMContentLoaded', main)
-
 for (let i = 0; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log('fizzbuzz')
-  }
-  if (i % 3 === 0) {
+  } else if (i % 3 === 0) {
     console.log('fizz')
-  }
-  if (i % 5 === 0) {
+  } else if (i % 5 === 0) {
     console.log('buzz')
   } else {
     console.log(i)
